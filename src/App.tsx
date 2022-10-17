db89f2d44d7b97083b51c68307470f789aa80ff4
Julio Cesar <favela2k20@gmail.com>
2022-10-17 18:24:24 -0300

    removed router component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import "./styles/global.css";
-import { SignIn } from "./pages/SignIn";
 import { Alert } from "./components/Alert";
 import { Header } from "./components/Header";
-import { FormsFooter } from "./components/FormsFooter";
 import { Container } from "./components/Container";
-import { BrowserRouter } from "react-router-dom";
 import { Routes } from "./Routes";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Container>
-        <Alert />
-        <Header />
-        <Routes />
-      </Container>
-    </BrowserRouter>
+    <Container>
+      <Alert />
+      <Header />
+      <Routes />
+    </Container>
   );
 }
 
