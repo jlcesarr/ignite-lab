395c99181df112cce1ca3ef37736f118a46fd547
Julio Cesar <favela2k20@gmail.com>
2022-10-17 17:37:35 -0300

    set default route to /login

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,13 @@
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch, Redirect } from "react-router-dom";
+import { Register } from "./pages/Register";
 import { SignIn } from "./pages/SignIn";
 
 function Routes() {
   return (
     <Switch>
+      <Route exact path="/">
+        <Redirect to="/login" />
+      </Route>
       <Route exact path="/login" component={SignIn}></Route>
     </Switch>
   );
