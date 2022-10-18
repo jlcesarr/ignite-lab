15f5238d727bad6d143219370897ba3c4f0670d5
Julio Cesar <favela2k20@gmail.com>
2022-10-18 18:47:21 -0300

    modified link component to have dynamic styles
    add logic to switch between native link or router dom

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -3,18 +3,22 @@ import {
   LinkProps as RouterLinkProps,
 } from "react-router-dom";
 import { ReactNode } from "react";
+import clsx from "clsx";
 
 export interface LinkProps extends RouterLinkProps {
   children: ReactNode;
   routerContext?: boolean;
 }
 
-function Link({ children, routerContext, ...props }: LinkProps) {
+function Link({ children, routerContext, className, ...props }: LinkProps) {
   const LinkType: any = routerContext ? RouterLink : "a";
 
   return (
     <LinkType
-      className="text-gray-400 text-xs underline hover:cursor-pointer hover:text-gray-200 transition-colors"
+      className={clsx(
+        "text-gray-400 text-xs underline hover:cursor-pointer hover:text-gray-200 transition-colors",
+        className
+      )}
       {...props}
     >
       {children}
